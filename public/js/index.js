786b2a7a44e7289d3bcab6b3a60bf6db8b0a2291
edumarrero <edumarreroglezz@gmail.com>
2025-02-23 02:25:24 +0100

    All car JSON loaded!

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 import { includeHTML } from "./main.js";
 import { loadHeaderContent } from "./header.js";
+import { populateCarSelect } from "./bookingBar.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await includeHTML();
     loadHeaderContent();
+    await populateCarSelect();
 });
