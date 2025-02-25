ba25c3901c820887258a1695d01b7776203abd64
Walo <wail.benelhassane2@gmail.com>
2025-02-25 00:09:01 +0000

    feat: add dynamic catalog card rendering from JSON

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -1,7 +1,9 @@
 import { includeHTML } from "./main.js";
 import { loadHeaderContent } from "./header.js";
+import { loadCatalogCards } from "./catalogCard.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await includeHTML();
     await loadHeaderContent();
+    await loadCatalogCards();
 });
