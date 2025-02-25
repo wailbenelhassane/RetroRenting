538ae1508391c833786cfda32302473a34fa7b62
Walo <wail.benelhassane2@gmail.com>
2025-02-25 02:44:06 +0000

    dev: functional carousel

--- public/js/aboutUs.js
+++ public/js/aboutUs.js
@@ -1,9 +1,11 @@
 import { includeHTML } from "./main.js";
 import { loadHeaderContent } from "./header.js";
 import { getReviews } from './reviews.js';
+import { initTeamCarousel } from './team-carousel.js';
 
 document.addEventListener("DOMContentLoaded", async () => {
   await includeHTML();
   await loadHeaderContent();
+  await initTeamCarousel();
   await getReviews();
 });
