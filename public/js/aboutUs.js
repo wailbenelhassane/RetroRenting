78a9772faef543b8fe7d48b1a90714586e849d8b
Walo <wail.benelhassane2@gmail.com>
2025-02-25 18:26:22 +0000

    dev: added center text section

--- public/js/aboutUs.js
+++ public/js/aboutUs.js
@@ -1,11 +1,14 @@
-import { includeHTML } from "./main.js";
+import {fetchJSON, includeHTML} from "./main.js";
 import { loadHeaderContent } from "./header.js";
 import { getReviews } from './reviews.js';
 import { initTeamCarousel } from './team-carousel.js';
+import {loadTextSections} from "./textSection.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await includeHTML();
+  await fetchJSON();
   await loadHeaderContent();
   await initTeamCarousel();
   await getReviews();
+  await loadTextSections();
 });
