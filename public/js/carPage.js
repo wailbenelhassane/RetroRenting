85dcfcb16f09b6948693408817c38f18cd022c81
Walo <wail.benelhassane2@gmail.com>
2025-02-27 12:34:29 +0000

    dev: car-viewer with image change

--- public/js/carPage.js
+++ public/js/carPage.js
@@ -1,7 +1,9 @@
 import { includeHTML } from "./main.js";
 import { loadHeaderContent } from "./header.js";
+import {changeImage} from "./changeImage.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await includeHTML();
     await loadHeaderContent();
+    await changeImage();
 });
