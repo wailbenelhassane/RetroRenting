c601ff0adca34b25f95b239ecd7a71d23f7a8e18
edumarrero <edumarreroglezz@gmail.com>
2025-03-01 15:41:40 +0100

    bar-car-details-booking WIP!

--- public/js/carPage.js
+++ public/js/carPage.js
@@ -1,11 +1,13 @@
 import { includeHTML } from "./main.js";
 import { loadHeaderContent } from "./header.js";
-import {changeImage} from "./changeImage.js";
-import {loadCarDetails} from "./carDetails.js";
+import { changeImage } from "./changeImage.js";
+import { loadCarDetails } from "./carDetails.js";
+import { barCarDetails } from "./barCarDetailsBooking.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await includeHTML();
     await loadHeaderContent();
     await loadCarDetails();
     await changeImage();
+    await barCarDetails();
 });
