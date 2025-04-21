c3fb04ce254e83231929826ba477d291a432a896
Walo <wailbenelhassane@gmail.com>
2025-04-21 19:32:44 +0100

    Feat: center-text-section load from database

--- src/app/components/center-text-section/center-text-section.component.ts
+++ src/app/components/center-text-section/center-text-section.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CenterTextSectionService } from '../../services/center-text-section.service';
-import {CommonModule} from '@angular/common';
+import { CenterTextData } from '../../models/center-text-section-model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-center-text-section',
   templateUrl: './center-text-section.component.html',
-  imports:[CommonModule],
   standalone: true,
+  imports: [CommonModule],
   styleUrls: ['./center-text-section.component.scss']
 })
-export class CenterTextSectionComponent implements OnInit {
+export class CenterTextSectionComponent implements OnInit, OnDestroy {
   @Input() sectionKey: string = '';
-  sectionData: any;
+  sectionData: CenterTextData = {};
+  private subscription: Subscription = new Subscription();
 
   constructor(private centerTextSectionService: CenterTextSectionService) {}
 
   ngOnInit() {
-    this.centerTextSectionService.getSectionData().subscribe(data => {
-      this.sectionData = data;
-    });
+    this.subscription.add(
+      this.centerTextSectionService.getSectionData().subscribe({
+        next: (data: CenterTextData) => {
+          this.sectionData = data;
+        },
+        error: (error: any) => {
+          console.error('Error loading center text data:', error);
+          this.sectionData = {};
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
