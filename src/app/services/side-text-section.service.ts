df48255dabf43ab0211bc42d187457b3959ee192
Walo <wailbenelhassane@gmail.com>
2025-04-21 19:46:16 +0100

    Feat: side-text-section load from database

--- src/app/services/side-text-section.service.ts
+++ src/app/services/side-text-section.service.ts
@@ -1,45 +1,49 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Injectable, OnDestroy, NgZone } from '@angular/core';
+import { Firestore, collection, getDocs, QuerySnapshot, DocumentData } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { SideTextData } from '../models/side-text-section.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SideTextSectionService {
-  private jsonUrl = '/data/sideText.json';
-  private sectionDataSubject = new BehaviorSubject<{ [key: string]: { title: string, content: string, src: string, alt: string } }>({});
+export class SideTextSectionService implements OnDestroy {
+  private sectionDataSubject = new BehaviorSubject<SideTextData>({});
   sectionData$ = this.sectionDataSubject.asObservable();
+  private destroy$ = new Subject<void>();
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private firestore: Firestore,
+    private ngZone: NgZone
+  ) {
     this.loadData();
   }
 
-  private getData(): Observable<any> {
-    return this.http.get<any>(this.jsonUrl).pipe(
-      catchError(error => {
-        console.error("Error loading side text section data:", error);
-        return throwError(error);
-      })
-    );
-  }
-
   private loadData() {
-    this.getData().subscribe({
-      next: (data) => {
-        if (data && data.sections) {
-          this.sectionDataSubject.next(data.sections);
+    this.ngZone.run(() => {
+      const sideTextCollection = collection(this.firestore, 'sideText');
+      getDocs(sideTextCollection).then((querySnapshot: QuerySnapshot<DocumentData>) => {
+        if (querySnapshot && !querySnapshot.empty) {
+          // Asumimos que el primer documento contiene todas las secciones
+          const sideTextData = querySnapshot.docs[0].data() as SideTextData;
+          this.sectionDataSubject.next(sideTextData);
         } else {
-          console.error("No sections found in the JSON data");
+          console.warn('No side text data found in Firestore');
+          this.sectionDataSubject.next({});
         }
-      },
-      error: (error) => {
-        console.error(error);
-      }
+      }).catch((error: any) => {
+        console.error('Error loading side text data from Firestore:', error);
+        this.sectionDataSubject.next({});
+      });
     });
   }
 
-  getSectionData() {
+  getSectionData(): Observable<SideTextData> {
     return this.sectionData$;
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
