d3c37cc5c3f4002e3978049facf1cd5f38bb4d6b
Walo <wailbenelhassane@gmail.com>
2025-05-03 13:27:54 +0100

    Feat: Add footer pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ import {RegisterComponent} from './pages/register/register.component';
 import {AboutUsComponent} from './pages/about-us/about-us.component';
 import {CarReservationConfirmationComponent} from './pages/car-reservation-confirmation/car-reservation-confirmation.component';
 import {CarReservationInformationComponent} from './pages/car-reservation-information/car-reservation-information.component';
+import {AccessibilityComponent} from './pages/footer-pages/accesibility/accesibility.component';
+import {PrivacyPolicyComponent} from './pages/footer-pages/privacy-policy/privacy-policy.component';
+import {AdChoicesComponent} from './pages/footer-pages/ad-choices/ad-choices.component';
+import {TermsConditionsComponent} from './pages/footer-pages/terms-conditions/terms-conditions.component';
 
 export const routes: Routes = [
   { path: 'index', component: LandingPageComponent },
@@ -15,5 +19,9 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'car-reservation-confirmation', component: CarReservationConfirmationComponent },
   { path: 'car-reservation-information', component: CarReservationInformationComponent },
+  { path: 'accessibility', component: AccessibilityComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'ad-choices', component: AdChoicesComponent },
+  { path: 'terms-conditions', component: TermsConditionsComponent },
   { path: '**', redirectTo: 'index' }
 ];
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,12 +3,13 @@ import { FooterService } from '../../services/footer.service';
 import { FooterItem } from '../../models/footer.model';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit, OnDestroy {
