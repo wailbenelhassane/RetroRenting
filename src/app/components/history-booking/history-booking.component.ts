f5569a8fb4d3967f7b8c74f68c50eb6c28b53751
edumarrero <edumarreroglezz@gmail.com>
2025-05-04 22:16:36 +0200

    worky worky!

--- src/app/components/history-booking/history-booking.component.ts
+++ src/app/components/history-booking/history-booking.component.ts
@@ -4,6 +4,8 @@ import { Booking } from '../../models/booking.model';
 import {NgForOf, NgIf} from '@angular/common';
 import {HeaderComponent} from '../header/header.component';
 import {FooterComponent} from '../footer/footer.component';
+import {AuthService} from '../../services/auth/auth.service';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-booking-history',
@@ -19,13 +21,14 @@ import {FooterComponent} from '../footer/footer.component';
 export class HistoryBookingComponent implements OnInit {
   bookings: Booking[] = [];
 
-  constructor(private bookingService: BookingsService) {}
+  constructor(private bookingService: BookingsService,
+              private afAuth: AngularFireAuth) {}
 
   ngOnInit(): void {
-    this.loadBookings();
-  }
-
-  async loadBookings() {
-    this.bookings = await this.bookingService.getByCurrentUser();
+    this.afAuth.authState.subscribe(async user => {
+      if (user) {
+        this.bookings = await this.bookingService.getByCurrentUser(user.uid)
+      }
+    })
   }
 }
--- src/app/services/firebase/booking-firebase.service.ts
+++ src/app/services/firebase/booking-firebase.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
-import {FirebaseService} from './firebase.service';
-import {Booking} from '../../models/booking.model';
-import {getAuth} from 'firebase/auth';
-import {collection, getDocs, query, where} from '@angular/fire/firestore';
+import { FirebaseService } from './firebase.service';
+import { Booking } from '../../models/booking.model';
+import { collection, getDocs, query, where } from '@angular/fire/firestore';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingsService extends FirebaseService<Booking> {
 
-  constructor() {
+  constructor(private authService: AuthService) {
     super('bookings');
   }
 
-  async getByCurrentUser(): Promise<Booking[]> {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    if (!user) return [];
+  async getByCurrentUser(uid: string): Promise<Booking[]> {
+    console.log(uid);
+    if (uid) {
+      const colRef = collection(this.firestore, 'bookings');
+      const q = query(colRef, where('userId', '==', uid));
+      const snapshot = await getDocs(q);
+      console.log(snapshot);
+      console.log(snapshot.docs.map(doc => ({id: doc.id, ...doc.data()} as Booking)));
+      return snapshot.docs.map(doc => ({id: doc.id, ...doc.data()} as Booking));
+    }
 
-    const colRef = collection(this.firestore, 'bookings');
-    const q = query(colRef, where('userId', '==', user.uid));
-    const snapshot = await getDocs(q);
-    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Booking));
+    return [];
   }
 }
