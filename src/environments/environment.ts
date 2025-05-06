0f6faf42e02ce4324c5f95708e5b70951e8f7e33
Dex <edumarreroglezz@gmail.com>
2025-05-06 18:28:24 +0100

    environment added!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  production: false,
+  firebaseConfig: { projectId: "panelon-fb7af",
+    appId: "1:539993197273:web:bd60dd54cfcc9f2c38ccc8",
+    storageBucket: "panelon-fb7af.firebasestorage.app",
+    apiKey: "AIzaSyCQqzf1Ue3v0WtdWNQ9GkP-TTta4eoW8hI",
+    authDomain: "panelon-fb7af.firebaseapp.com",
+    messagingSenderId: "539993197273"
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ import { AppComponent } from './app/app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {environment} from "./environments/environment";
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "panelon-fb7af", appId: "1:539993197273:web:bd60dd54cfcc9f2c38ccc8", storageBucket: "panelon-fb7af.firebasestorage.app", apiKey: "AIzaSyCQqzf1Ue3v0WtdWNQ9GkP-TTta4eoW8hI", authDomain: "panelon-fb7af.firebaseapp.com", messagingSenderId: "539993197273" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
