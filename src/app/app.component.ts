d3101af183c774c0f59aee84b17faa1e1e5ecd9d
Walo <wailbenelhassane@gmail.com>
2025-05-16 23:00:49 +0100

    Fix: StatusBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,14 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   standalone: true
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initializeStatusBar();
+  }
+
+  async initializeStatusBar() {
+    if (Capacitor.getPlatform() !== 'web') {
+      await StatusBar.setOverlaysWebView({ overlay: false });
+      await StatusBar.setStyle({ style: Style.Light });
+    }
+  }
 }
