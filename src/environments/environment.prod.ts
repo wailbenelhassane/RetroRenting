a4faf6ca7fab91bde9b713f4a3aa4516c2f7ce38
Carlos Ruano <carlosruanoramos@gmail.com>
2025-05-16 22:30:24 +0100

    Silly changes :P

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBpqrwIu8R59vzvHdAVbopJ1qApKxXK3Us",
+    authDomain: "retrorenting-acd8d.firebaseapp.com",
+    databaseURL: "https://retrorenting-acd8d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "retrorenting-acd8d",
+    storageBucket: "retrorenting-acd8d.firebasestorage.app",
+    messagingSenderId: "144755494629",
+    appId: "1:144755494629:web:4cf9bf2246eeabbfd8ce0c"
+  }
+}
